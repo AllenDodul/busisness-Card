2abf4404c6797b2baf9d79fd3d479c638ca65005
AllenDodul <allendodul6@gmail.com>
2022-08-15 12:56:28 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-wqcl15
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-wqcl15)
---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style';
+
+const Card = () =>{
+  return(
+    <div className="card">
+      <img
+        className="profile-picture" 
+        src="https://i.ibb.co/s6fxGsc/images-66.jpg"
+        alt="profile-picture"
+      />
+      <div></div>
+    </div>
+  )
+}
+export default Card;
